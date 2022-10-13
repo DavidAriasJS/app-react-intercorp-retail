4c5c3a0db7dff5b23baad46996f3bdf721fedc44
David Arias <bariasinv@gmail.com>
2022-10-12 22:43:51 -0500

    feat: add image logo

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react';
 
+import logo from './../assets/logo.svg';
+
 export const HomePage:FC = () => (
     <>  
         <br/>
         <h1>Reto Intercorp</h1>
         <div className="text-center p-10">
-            <img src="public/logo.svg" className="img-fluid" alt="Intercorp Retail" />
+            <img src={logo} className="img-fluid" alt="Intercorp Retail" />
         </div>
     </>
 );
